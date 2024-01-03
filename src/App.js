0c0989e527476364360bb80782190535c5826de0
seaweed78 <adhikarikshitiz12@gmail.com>
2024-01-03 11:08:41 +0545

    Add step

--- src/App.js
+++ src/App.js
@@ -7,14 +7,29 @@ export default function App() {
 
 function Counter() {
   const [count, setCount] = useState(0);
+  const [step, setStep] = useState(1);
+
+  const date = new Date("Jan 3 2024");
+  date.setDate(date.getDate() + count);
 
   return (
     <div className="container">
       <div className="mainBox">
         <div className="subBox">
+          <button className="decreaseBtn" onClick={() => setStep((s) => s - 1)}>
+            {" "}
+            -{" "}
+          </button>
+          <p>Step : {step}</p>
+          <button className="increaseBtn" onClick={() => setStep((s) => s + 1)}>
+            {" "}
+            +{" "}
+          </button>
+        </div>
+        <div className="subBox">
           <button
             className="decreaseBtn"
-            onClick={() => setCount((c) => c - 1)}
+            onClick={() => setCount((c) => c - step)}
           >
             {" "}
             -{" "}
@@ -22,14 +37,21 @@ function Counter() {
           <p>Count : {count}</p>
           <button
             className="increaseBtn"
-            onClick={() => setCount((c) => c + 1)}
+            onClick={() => setCount((c) => c + step)}
           >
             {" "}
             +{" "}
           </button>
         </div>
         <div className="text">
-          <p>Today is date</p>
+          <span>
+            {count === 0
+              ? "Today is "
+              : count > 0
+              ? `${count} days from now is `
+              : `${count} days ago was`}
+          </span>
+          <span> is {date.toDateString()}</span>
         </div>
       </div>
     </div>
