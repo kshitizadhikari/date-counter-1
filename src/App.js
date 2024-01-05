31221e98c4fbc9143e1d49ab2f5d4906dc26e0cf
seaweed78 <adhikarikshitiz12@gmail.com>
2024-01-05 10:16:57 +0545

    Add RightCounter

--- src/App.js
+++ src/App.js
@@ -2,7 +2,21 @@ import { useState } from "react";
 import "./App.css";
 
 export default function App() {
-  return <Counter />;
+  return (
+    <div className="container">
+      <div className="heading">
+        <h1>Date Counter</h1>
+      </div>
+      <div className="row">
+        <div className="col leftCol">
+          <Counter />
+        </div>
+        <div className="col rightCol">
+          <RightCounter />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 function Counter() {
@@ -13,8 +27,11 @@ function Counter() {
   date.setDate(date.getDate() + count);
 
   return (
-    <div className="container">
+    <div>
       <div className="mainBox">
+        <div className="heading">
+          <h2>Date Counter 1</h2>
+        </div>
         <div className="subBox">
           <button className="decreaseBtn" onClick={() => setStep((s) => s - 1)}>
             {" "}
@@ -35,6 +52,68 @@ function Counter() {
             -{" "}
           </button>
           <p>Count : {count}</p>
+          <button
+            className="increaseBtn"
+            onClick={() => setCount((c) => c + step)}
+          >
+            {" "}
+            +{" "}
+          </button>
+        </div>
+        <div className="text">
+          <span>
+            {count === 0
+              ? "Today is "
+              : count > 0
+              ? `${count} days from now is `
+              : `${count} days ago was`}
+          </span>
+          <span> is {date.toDateString()}</span>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function RightCounter() {
+  const [count, setCount] = useState(0);
+  const [step, setStep] = useState(1);
+
+  const date = new Date("Jan 3 2024");
+  date.setDate(date.getDate() + count);
+
+  return (
+    <div>
+      <div className="mainBox">
+        <div className="heading">
+          <h2>Date Counter 2</h2>
+        </div>
+        <div className="subBox rightSubBox">
+          <input
+            type="range"
+            min="1"
+            max="10"
+            value={step}
+            onChange={(e) => setStep(Number(e.target.value))}
+          ></input>
+          <p>Step : {step}</p>
+        </div>
+        <div className="subBox">
+          <button
+            className="decreaseBtn"
+            onClick={() => setCount((c) => c - step)}
+          >
+            {" "}
+            -{" "}
+          </button>
+          <p>
+            Count :{" "}
+            <input
+              type="text"
+              value={count}
+              onChange={(e) => setCount(Number(e.target.value))}
+            ></input>
+          </p>
           <button
             className="increaseBtn"
             onClick={() => setCount((c) => c + step)}
